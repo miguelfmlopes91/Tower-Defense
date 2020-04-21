44fbfeb93adb2d42cbdfe1ccd15873929bf88405
Miguel Lopes <miguel.lopes@miniclip.com>
2020-04-21 19:29:23 +0100

    finish currency system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : Singleton<GameManager>
 {
     private int currency;
 
+
+
     [SerializeField]
     private Text currencyText;
 
@@ -15,10 +17,17 @@ public class GameManager : Singleton<GameManager>
     {
         get;set;
     }
+    public int Currency { get => currency;
+        set {
+            currency = value;
+            currencyText.text = value.ToString() + "<color=lime>$</color>";
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Currency = 5;
     }
 
     // Update is called once per frame
@@ -28,12 +37,21 @@ public class GameManager : Singleton<GameManager>
     }
 
     public void PickTower(TowerButton tower) {
-        TowerClickButton = tower;
-        Hover.Instance.Activate(tower.TowerSprite);
+
+        if (currency>=tower.Price)
+        {
+            TowerClickButton = tower;
+            Hover.Instance.Activate(tower.TowerSprite);
+        }
+
     }
 
     public void BuyTower() {
-        Hover.Instance.Deactivate();
+        if (Currency >= TowerClickButton.Price)
+        {
+            Currency -= TowerClickButton.Price;
+            Hover.Instance.Deactivate();
+        }
     }
 
     private void HandleEscape()
--- Assets/Scripts/TowerButton.cs
+++ Assets/Scripts/TowerButton.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerButton : MonoBehaviour
 {
@@ -9,8 +10,18 @@ public class TowerButton : MonoBehaviour
     private GameObject towerButtonPrefab;
     [SerializeField]
     private Sprite towerSprite;
+    [SerializeField]
+    private int price;
+    [SerializeField]
+    private Text priceText;
 
 
     public GameObject TowerButtonPrefab { get => towerButtonPrefab;}
     public Sprite TowerSprite { get => towerSprite; set => towerSprite = value; }
+    public int Price { get => price; set => price = value; }
+
+    private void Start()
+    {
+        priceText.text = Price + "$";
+    }
 }
